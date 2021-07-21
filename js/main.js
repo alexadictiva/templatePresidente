6ef107583e908fc2c727c6b4ce54301b0c076a08
Libia Perez <alexandra220790@gmail.com>
2021-07-20 23:00:31 -0300

    Agregando estilos a todas las secciones y armando el menu para mobile y desktop

--- js/main.js
+++ js/main.js
@@ -3,9 +3,32 @@ function toggleMenu() {
   menu.classList.toggle("show");
 }
 
-var swiper = new Swiper(".mySwiper", {
+var swiper1 = new Swiper(".mySwiper1", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
   },
 });
